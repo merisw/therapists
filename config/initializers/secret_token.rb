d3291c0b0eab6b495325bb08012d81ab067449a0
Meris Williams <merisw@gmail.com>
2013-05-22 13:54:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Therapists::Application.config.secret_token = 'dc8cbcc6dc3925af7774d7dae2f005bdb768941e76b9deaabd83305e059492bde92c15a641a0d45c4bd2545ce7c290cee0518924fa74c5275d2e8a0279fe050f'
