4ffc463f9e714dcab232916f01a2e8188c8333d2
Sean <sean.t.irby@gmail.com>
2013-06-09 23:31:32 -0700

    Add validations and fix form error

--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -2,6 +2,6 @@ class Therapist < ActiveRecord::Base
   has_many :assignments
   has_many :schools, :through => :assignments
 
-  attr_accessible :email, :fTE, :name, :therapist_type
+  attr_accessible :email, :fte, :name, :therapist_type
   validates :name, :presence => true
 end
